87c29b47c0a61e84028db97c2c14f8bf4d70815c
ducle070106 <ducle070106@gmail.com>
2025-03-03 23:38:33 +0700

    Update b4.js

--- b4.js
+++ b4.js
@@ -1,15 +1,7 @@
-let num=+prompt("mời nhập vào số nguyên");
-if(Number.isInteger(num)){
-    if(num%3===0&&num%5===0){
-        alert(num+" chia hết cho cả 3 và 5");
-    }else if(num%3===0){
-        alert(num+" chia hết cho 3");
-    }else if(num%5===0){
-        alert(num+" chia hết cho 5");
-    }else{
-        alert(num+" không chia hết cho cả 3 và 5");
-    }
-}else{
-    alert("không phải số nguyên");
+let a = +prompt("nhap 1 so ");
+
+if (a % 5 == 0 && a % 3 == 0 && a != 0) {
+    console.log(a, "chia het cho ca 5 va 3");
+} else {
+    console.log(a, "khong chia het cho ca 5 va 3");
 }
-location.reload();
